7193bb7b0f7c3957d4ead0257284d06895486166
mattia cappa <mattia.cappa@studenti.unibs.it>
2026-04-08 12:41:33 +0200

    Aggiunti attributi a Corpo.java e creata classe Stella.java

--- Corpo.java
+++ Corpo.java
@@ -1,2 +1,16 @@
 public abstract class Corpo {
+    String id;
+    int massa=0, grado, coordX, coordY;
+
+    public String getId() {
+        return id;
+    }
+
+    public int getGrado() {
+        return grado;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
 }
---
+++ Stella.java
@@ -0,0 +1,6 @@
+public class Stella extends Corpo {
+
+    public Stella() {
+
+    }
+}
