92d166934d38a19b788b68c98c8ddc16cbae5bcd
mattia cappa <mattia.cappa@studenti.unibs.it>
2026-04-08 18:45:40 +0200

    Implementata logica e gestione della creazione della stella e dell'aggiunta dei pianeti.

--- Main.java
+++ Main.java
@@ -5,18 +5,38 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // variabile di controllo
+        boolean check = true;
+
         // Input dell'utente
         Scanner scanner = new Scanner(System.in);
         String UserInput;
 
-        // Punto di riferimento del sistema stellare
-        Stella stella;
+        // Punto di riferimento del sistema stellare (Stella)
+        Stella stella = null;
+        System.out.println("\nPrima di tutto dobbiamo andare a creare la STELLA attorno alla quale orbitano tutti i nostri pianeti: ");
+
+        while (check) {
+            try {
+                System.out.print("\nInserisci il nome della stella: ");
+                String id = scanner.nextLine();
+                System.out.print("\nInserisci la massa della stella: ");
+                int massa = Integer.parseInt(scanner.nextLine());
+                stella = new Stella(id, massa);
+
+                check = false;
+            } catch (Exception e) {
+                System.out.println("\n !!!!!!!!! Inserisci dei valori adeguati !!!!!!!!!");
+            }
+        }
+
+
 
 
         // Main loop
         stampaMenu();
         do{
-            System.out.print("-- ");
+            System.out.print("~home~ ");
             UserInput = scanner.nextLine();
 
             switch (UserInput.toLowerCase()){
@@ -26,7 +46,8 @@ public class Main {
                     break;
                 }
                 case("+corpo"):{
-
+                    aggiungiCorpo(stella);
+                    pulisciConsole();
                     break;
                 }
                 case("-corpo"):{
@@ -96,16 +117,95 @@ public class Main {
     private static void stampaMenu(){
         System.out.println("//////////////////");
         System.out.println("Benvenuto nel menù");
-        System.out.println("menu -> apri questa sezione");
-        System.out.println("esci -> esci dal programma");
-        System.out.println("+corpo -> crea stella/pianeta/luna");
-        System.out.println("-corpo -> romuovi stella/pianeta/luna");
+        System.out.println("menu   -> apri questa sezione");
+        System.out.println("esci   -> esci dal programma");
+        System.out.println("+corpo -> crea pianeta/luna");
+        System.out.println("-corpo -> rimuovi pianeta/luna");
+        System.out.println("cerca  -> cerca un determinato corpo");
         System.out.println("//////////////////");
     }
 
     // Aggiungi corpo
-    private void aggiungiCorpo(){
+    private static void aggiungiCorpo(Stella stella){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                pianeta  -> aggiungi un pianeta
+                luna     -> aggiungi una luna
+                indietro -> torna alla home""");
 
+        boolean check = true;
+        do {
+            System.out.print("~+~ ");
+            String UserInput = scanner.nextLine();
+            switch (UserInput.toLowerCase()){
+                case "pianeta":{
+                    check = false;
+                    aggiungiPianeta(stella);
+                    break;
+                }
+
+                case "luna":{
+                    check = false;
+                    aggiungiLuna();
+                    break;
+                }
+
+                case "indietro":
+                    check = false;
+                    break;
+
+                default:{
+                    System.out.println("Comando non trovato, puoi aggiungere un pianeta o una luna, oppure tornare indietro");
+                }
+            }
+
+        }while(check);
+
+    }
+
+    private static void aggiungiPianeta(Stella s) {
+        boolean check = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nInserisci il nome del pianeta: ");
+        String id = scanner.nextLine();
+        int massa = 0;
+        int coordX;
+        int coordY;
+
+        do {
+            try {
+                System.out.print("\nInserisci la massa del pianeta: ");
+                massa = Integer.parseInt(scanner.nextLine());
+                check = false;
+            }catch (Exception e){
+                System.out.println("\n !!!! Inserisci dei valori adeguati !!!!");
+                check = true;
+            }
+        }while (check);
+
+
+        do {
+            check = false;
+            System.out.print("\nInserisci la coordinata X del pianeta: ");
+            coordX = Integer.parseInt(scanner.nextLine());
+            System.out.print("\nInserisci la coordinata Y del pianeta: ");
+            coordY = Integer.parseInt(scanner.nextLine());
+            if((coordX < -100 || coordY < -100) || (coordX > 100 || coordY > 100 )) {
+                System.out.println("\n !!!! inserisci dei valori compresi tra -100 e 100 !!!!");
+                check = true;
+            }
+        }while (check);
+
+        s.pianeti.add(new Pianeta(id, massa, coordX, coordY));
+
+        System.out.println("pianeta aggiunto");
+    }
+
+    private static void aggiungiLuna() {
+
+
+
+        System.out.println("luna aggiunta");
     }
 
 
